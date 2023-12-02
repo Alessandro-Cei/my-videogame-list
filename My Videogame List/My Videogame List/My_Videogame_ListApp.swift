3782830a5a8b4e12a1cb5163775e9d990cfb16d2
Alessandro Cei <ale.steal94@gmail.com>
2023-12-02 12:03:05 +0100

    Initial commit

---
+++ My Videogame List/My Videogame List/My_Videogame_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Videogame_ListApp.swift
+//  My Videogame List
+//
+//  Created by alessandro on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Videogame_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
