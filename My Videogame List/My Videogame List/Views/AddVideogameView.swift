0abc92f259c21d661903dc418073e201f1de6457
Alessandro Cei <ale.steal94@gmail.com>
2023-12-04 01:12:01 +0100

    "Add" button added to addvideogameview

--- My Videogame List/My Videogame List/Views/AddVideogameView.swift
+++ My Videogame List/My Videogame List/Views/AddVideogameView.swift
@@ -16,6 +16,7 @@ struct AddVideogameView: View {
             HStack {
                 Spacer()
                 VStack (spacing: 10){
+                    Spacer()
                     Text(game.name)
                         .multilineTextAlignment(.leading)
                         .font(.largeTitle)
@@ -23,6 +24,18 @@ struct AddVideogameView: View {
                         .scaledToFit()
                         .frame(width: geo.size.width * 0.5, height: geo.size.width * 0.5)
                         .cornerRadius(20)
+                    Spacer()
+                    Spacer()
+                    Button(action: {
+                        //Add to database
+                    }, label: {
+                        Text("Add to collection")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.blue)
+                            .cornerRadius(10)
+                    })
                     Spacer()
                 }
                 .padding()
